338de4fbc32c9dbcc83feb6946f2711055ff7d5d
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-10-14 18:27:28 +0300

    done

--- task5/Program.cs
+++ task5/Program.cs
@@ -8,10 +8,13 @@ namespace task5
         {
             BankAccount account = new BankAccount(0, AccountType.Deposit);
             Console.WriteLine(account);
+            Console.WriteLine("Кладем на счет 50000");
             account.IncreaseBalance(50000);
             Console.WriteLine(account);
+            Console.WriteLine("Снимаем 10000");
             account.GetMoney(10000);
             Console.WriteLine(account);
+            Console.WriteLine("Снимаем 50000");
             account.GetMoney(50000);
             Console.WriteLine(account);
         }
